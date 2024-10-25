65784e6e64d46da42671428f2ab1847b8d667300
KyuHwan00 <hanbitchan@unist.ac.kr>
2024-10-25 14:42:41 +0900

    Update STDit logic for new converted models

--- On-Device-OpenSora/STDit3.swift
+++ On-Device-OpenSora/STDit3.swift
@@ -27,14 +27,11 @@ public struct STDit3 {
       try model.prediction(from: inputFeatures)
     }
     part1.unloadResources()
-//    var stdit3Part1: stdit3_part1? = try! stdit3_part1(configuration: config)
-//    let stdit3Part1Input = stdit3_part1Input(z_in: x, t: timestep, y: modelargs["y"]!, mask: modelargs["mask"]!, x_mask: modelargs["x_mask"]!, fps: modelargs["fps"]!, height: modelargs["height"]!, width: modelargs["width"]!)
-//    let stdit3Part1Output = try await stdit3Part1!.prediction(input: stdit3Part1Input)
+
     print("=== Done stdit3 Part1 ===")
-//    stdit3Part1 = nil
     
     inputFeatures = try MLDictionaryFeatureProvider (
-      dictionary : [ "x" : stdit3Part1Output.featureValue(for: "x"), "y" : stdit3Part1Output.featureValue(for: "outY"), "y_lens": stdit3Part1Output.featureValue(for: "y_lens"), "mask_expanded": stdit3Part1Output.featureValue(for: "mask_expanded") ,"t_mlp": stdit3Part1Output.featureValue(for: "t_mlp"), "x_mask": MLMultiArray(modelargs["x_mask"]!), "t0_mlp": stdit3Part1Output.featureValue(for: "t0_mlp"), "T": stdit3Part1Output.featureValue(for: "T"), "S": stdit3Part1Output.featureValue(for: "S")]
+      dictionary : [ "x" : stdit3Part1Output.featureValue(for: "x")!, "y" : stdit3Part1Output.featureValue(for: "outY")!, "y_lens": stdit3Part1Output.featureValue(for: "y_lens")!, "t_mlp": stdit3Part1Output.featureValue(for: "t_mlp")!, "x_mask": MLMultiArray(modelargs["x_mask"]!), "t0_mlp": stdit3Part1Output.featureValue(for: "t0_mlp")!, "T": stdit3Part1Output.featureValue(for: "T")!, "S": stdit3Part1Output.featureValue(for: "S")!]
     )
 
 
@@ -45,7 +42,7 @@ public struct STDit3 {
       }
       spatialAndTemporal.unloadResources()
       inputFeatures = try MLDictionaryFeatureProvider (
-        dictionary : [ "x" : spatialOutput.featureValue(for: "output"), "y" : stdit3Part1Output.featureValue(for: "outY"), "y_lens": stdit3Part1Output.featureValue(for: "y_lens"), "mask_expanded": stdit3Part1Output.featureValue(for: "mask_expanded"), "t_mlp": stdit3Part1Output.featureValue(for: "t_mlp"), "x_mask": MLMultiArray(modelargs["x_mask"]!), "t0_mlp": stdit3Part1Output.featureValue(for: "t0_mlp"), "T": stdit3Part1Output.featureValue(for: "T"), "S": stdit3Part1Output.featureValue(for: "S")]
+        dictionary : [ "x" : spatialOutput.featureValue(for: "output")!, "y" : stdit3Part1Output.featureValue(for: "outY")!, "y_lens": stdit3Part1Output.featureValue(for: "y_lens")!, "t_mlp": stdit3Part1Output.featureValue(for: "t_mlp")!, "x_mask": MLMultiArray(modelargs["x_mask"]!), "t0_mlp": stdit3Part1Output.featureValue(for: "t0_mlp")!, "T": stdit3Part1Output.featureValue(for: "T")!, "S": stdit3Part1Output.featureValue(for: "S")!]
       )
 //      var output = [Float32]()
 //      for i in 0...100 {
@@ -57,17 +54,12 @@ public struct STDit3 {
     
     // === final layer ===
     inputFeatures = try MLDictionaryFeatureProvider(dictionary: ["x": (inputFeatures.featureValue(for: "x")?.multiArrayValue!)!, "x_mask": MLMultiArray(modelargs["x_mask"]!), "t": (stdit3Part1Output.featureValue(for: "outT")?.multiArrayValue!)!, "t0": (stdit3Part1Output.featureValue(for: "t0")?.multiArrayValue!)!, "T": (stdit3Part1Output.featureValue(for: "T")?.multiArrayValue!)!, "S": (stdit3Part1Output.featureValue(for: "S")?.multiArrayValue!)!, "H": (stdit3Part1Output.featureValue(for: "H")?.multiArrayValue!)!,"W": (stdit3Part1Output.featureValue(for: "W")?.multiArrayValue!)!, "Tx": (stdit3Part1Output.featureValue(for: "Tx")?.multiArrayValue!)!, "Hx": (stdit3Part1Output.featureValue(for: "Hx")?.multiArrayValue!)!, "Wx": (stdit3Part1Output.featureValue(for: "Wx")?.multiArrayValue!)!])
+    
     let stdit3Part2Output = try part2.perform { model in
       try model.prediction(from: inputFeatures)
     }
     part2.unloadResources()
-      
-//    var stdit3Part2: stdit3_part2? = try! stdit3_part2(configuration: config)
-//    let stdit3Part2Input = stdit3_part2Input(x: (inputFeatures.featureValue(for: "x")?.multiArrayValue!)!, x_mask: (stdit3Part1Output.featureValue(for: "x_mask")?.multiArrayValue!)!, t: (stdit3Part1Output.featureValue(for: "outT")?.multiArrayValue!)!, t0: (stdit3Part1Output.featureValue(for: "t0")?.multiArrayValue!)!, T: (stdit3Part1Output.featureValue(for: "T")?.multiArrayValue!)!, S: (stdit3Part1Output.featureValue(for: "S")?.multiArrayValue!)!, H: (stdit3Part1Output.featureValue(for: "H")?.multiArrayValue!)!, W: (stdit3Part1Output.featureValue(for: "W")?.multiArrayValue!)!, Tx: (stdit3Part1Output.featureValue(for: "Tx")?.multiArrayValue!)!, Hx: (stdit3Part1Output.featureValue(for: "Hx")?.multiArrayValue!)!, Wx: (stdit3Part1Output.featureValue(for: "Wx")?.multiArrayValue!)!)
-//    let stdit3Part2Output = try await stdit3Part2!.prediction(input: stdit3Part2Input)
-//    stdit3Part2 = nil
-//    print("=== Done stdit3 Part2 ===")
-//
+    
     let output = MLTensor(MLShapedArray<Float32>((stdit3Part2Output.featureValue(for: "output")?.multiArrayValue)!))
     return output
   }
